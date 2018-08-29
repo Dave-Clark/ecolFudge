1e5daab1b34452968750e662ae016747549112c1
Dave Clark PGR <dclarkb@essex.ac.uk>
2018-08-29 17:13:32 +0100

    sorted structure

--- ecolFudge/R/transDT.R
+++
@@ -1,8 +0,0 @@
-transDT <- function(dt, transCol, rowID){
-  newRowNames <- colnames(dt)
-  newColNames <- dt[, transCol, with = F]
-  transposedDt <- transpose(dt[, !colnames(dt) %in% transCol, with = F])
-  colnames(transposedDt) <- unlist(newColNames)
-  transposedDt[, (rowID) := newRowNames[newRowNames != transCol]]
-  return(transposedDt)
-}
