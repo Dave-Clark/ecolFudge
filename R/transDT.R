1f15001229acad3bba31cc1bf185661190aeca9e
Dave Clark <dclarkb@essex.ac.uk>
2020-07-29 11:19:32 +0100

    Started adding package documentation

--- R/transDT.R
+++ R/transDT.R
@@ -1,3 +1,14 @@
+#' A function for quickly transposing data.table objects
+#'
+#' This function allows you transpose data.table objects very rapidly, whilst maintaining control of row and column names.
+#' @param dt The data.table object you wish to transpose.
+#' @param transCol The name of the column that you wish to pivot on. Values in this column will become the new colnames.
+#' @param rowID This will be the name of the new rownames column
+#' @keywords data.table
+#' @export
+#' @examples
+#' transDT()
+
 transDT <- function(dt, transCol, rowID){
   newRowNames <- colnames(dt)
   newColNames <- dt[, transCol, with = F]
